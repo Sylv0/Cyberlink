ad8b9573e05d50852b194a3c0606826d67908931
Johan Sylvan <gtc124@gmail.com>
2017-12-06 23:00:54 +0100

    Changed/improved navbar

--- templates/navbar.php
+++ templates/navbar.php
@@ -4,20 +4,27 @@
         aria-expanded="false" aria-label="Toggle navigation"></button>
     <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+                <a class="nav-link" href=".">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Make Post</a>
+                <a class="nav-link" href="<?php echo isset($_SESSION['user_id']) ? "makepost.php" : "login.php"; ?>">Make Post</a>
             </li>
-            <li class="nav-item dropdown">
+            <!-- <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
                 <div class="dropdown-menu" aria-labelledby="dropdownId">
                     <a class="dropdown-item" href="#">Action 1</a>
                     <a class="dropdown-item" href="#">Action 2</a>
                 </div>
+            </li> -->
+        </ul>
+        <?php if(!isset($_SESSION['user_id'])): ?>
+        <ul class="navbar-nav navbar-right">
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Login</a>
             </li>
         </ul>
+        <?php endif; ?>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search for post">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
