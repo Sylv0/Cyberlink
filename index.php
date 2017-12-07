75749ace3164bb550f775f3bfb3c33a14f1b50a0
Johan Sylvan <gtc124@gmail.com>
2017-12-07 13:10:05 +0100

    Added test-"posts" to frontpage

--- index.php
+++ index.php
@@ -13,7 +13,25 @@
     <?php require(__DIR__.'/templates/navbar.php'); ?>
 
     <main class="container">
-        
+    <div class="row">
+            <div class="col-xs-12 col-sm-10 col-lg-11 post-container">
+                <div>
+                    <h3>Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas assumenda reiciendis ratione cumque atque adipisci, iure quas cupiditate quos quia laudantium nisi illo et quibusdam deleniti labore temporibus, earum repellendus?</p>
+                </div>
+                <hr>
+                <div>
+                    <h3>Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas assumenda reiciendis ratione cumque atque adipisci, iure quas cupiditate quos quia laudantium nisi illo et quibusdam deleniti labore temporibus, earum repellendus?</p>
+                </div>
+                <hr>
+                <div>
+                    <h3>Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas assumenda reiciendis ratione cumque atque adipisci, iure quas cupiditate quos quia laudantium nisi illo et quibusdam deleniti labore temporibus, earum repellendus?</p>
+                </div>
+                <hr>
+            </div>
+        </div>
     </main>
 
     <!-- Optional JavaScript -->
