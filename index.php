687319e6608cde393c8baedca5e5ce11523575c1
Johan Sylvan <gtc124@gmail.com>
2017-12-24 01:10:13 +0100

    Added testpost to try and find a good look

--- index.php
+++ index.php
@@ -2,24 +2,45 @@
 declare(strict_types=1);
 require(__DIR__.'/views/header.php');
 ?>
-<main class="container">
+<main class="container-fluid">
     <div class="row">
-        <div class="col-xs-12 col-sm-10 col-lg-11 post-container">
-            <div>
-                <h3>Title</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas assumenda reiciendis ratione cumque atque adipisci, iure quas cupiditate quos quia laudantium nisi illo et quibusdam deleniti labore temporibus, earum repellendus?</p>
+        <div class="col-xs-12 col-sm-10 col-lg-10 post-container">
+            <div class="card mt-4">
+                <div class="card-header">
+                    Title
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis numquam iusto quasi praesentium odit. Error cumque sunt eligendi, aspernatur neque asperiores, rem, laudantium tempora minus nulla maiores. Error, modi delectus.</p>
+                        <footer class="blockquote-footer">Author <cite title="Source Title">Date</cite> (*edit)</footer>
+                    </blockquote>
+                </div>
             </div>
-            <hr>
-            <div>
-                <h3>Title</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas assumenda reiciendis ratione cumque atque adipisci, iure quas cupiditate quos quia laudantium nisi illo et quibusdam deleniti labore temporibus, earum repellendus?</p>
+        </div>
+        <div class="col-xs-12 col-sm-10 col-lg-10 post-container">
+            <div class="card mt-4">
+                <div class="card-header">
+                    Title
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis numquam iusto quasi praesentium odit. Error cumque sunt eligendi, aspernatur neque asperiores, rem, laudantium tempora minus nulla maiores. Error, modi delectus.</p>
+                        <footer class="blockquote-footer">Author <cite title="Source Title">Date</cite> (*edit)</footer>
+                    </blockquote>
+                </div>
             </div>
-            <hr>
-            <div>
-                <h3>Title</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum temporibus adipisci alias expedita, itaque vitae aspernatur? A hic voluptatum doloremque, temporibus impedit ducimus, amet labore quo libero perspiciatis repudiandae?</p>
+        </div><div class="col-xs-12 col-sm-10 col-lg-10 post-container">
+            <div class="card mt-4">
+                <div class="card-header">
+                    Title
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis numquam iusto quasi praesentium odit. Error cumque sunt eligendi, aspernatur neque asperiores, rem, laudantium tempora minus nulla maiores. Error, modi delectus.</p>
+                        <footer class="blockquote-footer">Author <cite title="Source Title">Date</cite> (*edit)</footer>
+                    </blockquote>
+                </div>
             </div>
-            <hr>
         </div>
     </div>
 </main>
