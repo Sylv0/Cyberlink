0c9d0a6339d55fde59d38bb731c1cdda32f33bb3
Johan Sylvan <gtc124@gmail.com>
2017-12-24 01:10:39 +0100

    Changed navbar links a bit and added link to profile

--- views/navbar.php
+++ views/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-light bg-light">
-    <a class="navbar-brand" href="#"><img src="assets/images/logo.svg" width="30" height="30" class="d-inline-block align-top" alt=""><span class="visible-sm">Cyberlink</span></a>
+    <a class="navbar-brand" href="./"><img src="assets/images/logo.svg" width="30" height="30" class="d-inline-block align-top" alt=""><span class="visible-sm">Cyberlink</span></a>
     <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
         aria-expanded="false" aria-label="Toggle navigation"></button>
     <div class="collapse navbar-collapse" id="collapsibleNavId">
@@ -14,14 +14,17 @@
         <?php if(!$loggedIn): ?>
         <ul class="navbar-nav ml-auto mt-2 mt-lg-0 justify-content-end">
             <li class="nav-item">
-                <a class="nav-link" href="login.php">Login</a>
+                <a class="nav-link text-primary" href="login.php">Login</a>
             </li>
         </ul>
         <?php endif; ?>
         <?php if($loggedIn): ?>
         <ul class="navbar-nav ml-auto mt-2 mt-lg-0 justify-content-end">
             <li class="nav-item">
-                <a class="nav-link" href="app/auth/logout.php">Log out</a>
+                <a class="nav-link text-primary" href="profile.php">(profile)</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-warning" href="app/auth/logout.php">Log out</a>
             </li>
         </ul>
         <?php endif; ?>
