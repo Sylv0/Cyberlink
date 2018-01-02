d5fb7193894fe342704025c3313cc961efeaadc1
Johan Sylvan <gtc124@gmail.com>
2018-01-02 19:05:42 +0100

    Changed some error logging

--- app/posts/get.php
+++ app/posts/get.php
@@ -8,16 +8,17 @@
     if(isset($_POST['getAll']) && $_POST['getAll']){
         $getPosts = $pdo->prepare('SELECT p.title, p.postText, p.postTime, p.updateTime, u.nickname AS author FROM posts AS p, users AS u WHERE p.authorID=u.id');
         if(!$getPosts->execute()){
-            echo "ERROR";
-            die;
             echo json_encode(["error"=>true, "errorInfo"=>"Couldn't get posts."]);
+            die;
         }
+        echo json_encode($getPosts->fetchAll(PDO::FETCH_ASSOC));
+        die;
     }else if(isset($_POST['getLatest']) && $_POST['getLatest']){
         $getPosts = $pdo->prepare('SELECT p.title, p.postText, p.postTime, p.updateTime, u.nickname AS author FROM posts AS p, users AS u WHERE p.authorID=u.id ORDER BY p.postID DESC LIMIT 1');
         if(!$getPosts->execute()){
-            echo "ERROR";
-            die;
             echo json_encode(["error"=>true, "errorInfo"=>"Couldn't get posts."]);
+            die;
         }
+        echo json_encode($getPosts->fetch(PDO::FETCH_ASSOC));
+        die;
     }
-    echo json_encode($getPosts->fetchAll(PDO::FETCH_ASSOC));
