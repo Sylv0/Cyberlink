9e960fd2cc124e6db67125a60469ab85257a4893
Johan Sylvan <gtc124@gmail.com>
2018-01-02 19:06:01 +0100

    Changed form to work better

--- index.php
+++ index.php
@@ -5,22 +5,25 @@ require(__DIR__.'/views/header.php');
 
 <main class="container-fluid">
     <?php if(isset($_SESSION['userid'])): ?>
-        <form name="postForm">
+        <form name="postForm" onsubmit="return false;" class="col-lg-10 col-md-12">
           <fieldset class="form-group">
             <label for="formGroupExampleInput">Title</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Your title" name="postTitle">
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Your title" name="postTitle" required>
           </fieldset>
           <fieldset class="form-group">
             <label for="formGroupExampleInput2">Text</label>
-            <textarea rows="4" type="text" class="form-control" id="formGroupExampleInput2" placeholder="Your text" name="postText"></textarea>
+            <textarea rows="4" type="text" class="form-control" id="formGroupExampleInput2" placeholder="Your text" name="postText" required></textarea>
           </fieldset>
           <input type="hidden" name="postAuthor" value="<?php echo $_SESSION['userid']; ?>">
           <input type="submit" name="postSubmit" value="Post" class="btn btn-primary">
         </form>
-    <?php endif; ?>
+        <script src="assets/scripts/posts.js" charset="utf-8"></script>
+    <?php
+else:
+    echo "Not logged in, please do.";
+    endif; ?>
     <div class="row posts">
 
     </div>
-    <script src="assets/scripts/posts.js" charset="utf-8"></script>
 </main>
 <?php require(__DIR__.'/views/footer.php'); ?>
