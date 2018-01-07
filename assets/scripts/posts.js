b878f118bb2db649f4dd17e0371ba52b532eee35
Johan Sylvan <gtc124@gmail.com>
2018-01-07 20:26:36 +0100

    Started adding system for voting

--- assets/scripts/posts.js
+++ assets/scripts/posts.js
@@ -10,11 +10,13 @@ window.onload = () => {
     });
 
     getPosts();
+    getVotes();
 }
 
 const submitPost = () => {
     let formData = new FormData();
     formData.append('title', postForm['postTitle'].value);
+    formData.append('link', postForm['postLink'].value);
     formData.append('text', postForm['postText'].value);
     formData.append('author', postForm['postAuthor'].value);
     var request = new Request('app/posts/submit.php', {
@@ -46,15 +48,21 @@ const createPost = (post) => {
         <a href="${post['imageURL']}" class="card-header" onclick="return false;">
             ${post['title']}
         </a>
-        <div class="card-body">
-            <blockquote class="blockquote mb-0">
+        <div class="card-body row">
+            <blockquote class="blockquote mb-0 col-10">
             <p>${post['postText']}</p>
             <footer class="blockquote-footer"><a href="${post['author']}">${post['author']}</a> <cite title="Source Title">${post['postTime']}</cite> (${post['updateTime']})</footer>
             </blockquote>
+            <blockquote class="col-2"><a href="#" class="btn btn-sm btn-primary voteUp">&uArr;</a> 0 <a href="#" class="btn btn-sm btn-primary voteUp">&dArr;</a></blockquote>
         </div>
     </div>
     `;
     div.innerHTML = postTemp;
+    if(post['imageURL'] == undefined){
+        div.querySelector('a.card-header').setAttribute('href', '#');
+        div.querySelector('a.card-header').setAttribute('onclick', 'return false;');
+        div.querySelector('a.card-header').innerHTML += "(no link)";
+    }
     return div;
 }
 
@@ -102,6 +110,24 @@ const getLatestPost = () => {
         } else {
             postDiv.insertBefore(createPost(data), postDiv.children[0]);
         }
+    })
+    .catch(res => console.log(res));
+}
+
+const getVotes = () => {
+    let formData = new FormData();
+    formData.append('getVotes', true);
+    var request = new Request('app/posts/get.php', {
+        method: 'POST',
+        body: formData
+    })
+
+    fetch(request)
+    .then(function (data) {
+        return data.json();
+    })
+    .then(data => {
+        console.log(data);
     })
     .catch(res => console.log(res));
 }
