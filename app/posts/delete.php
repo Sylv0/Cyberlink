ef246dc63933df87a3e12cdaa5b4d30c627a29fb
Johan Sylvan <gtc124@gmail.com>
2018-01-07 20:25:24 +0100

    Delete one of your posts

--- app/posts/delete.php
+++ app/posts/delete.php
@@ -5,3 +5,14 @@ declare(strict_types=1);
 require __DIR__.'/../autoload.php';
 
 // In this file we delete new posts in the database.
+if(!isset($_GET['post'])){
+    redirect('../../profile.php');
+}
+
+$removePost = $pdo->prepare("DELETE FROM posts WHERE postID = :id");
+$removePost->bindParam(':id', $_GET['post']);
+if(!$removePost->execute()){
+    echo "Something went wrong<br><a href='../../profile.php'>Go back</a>";
+}
+
+redirect('../../profile.php');
