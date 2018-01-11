36b81a8f5132e5f1663fe7ebfc6aac77c6b1bf1f
Johan Sylvan <gtc124@gmail.com>
2018-01-11 09:09:10 +0100

    Made placeholders better?

--- index.php
+++ index.php
@@ -8,15 +8,15 @@ require(__DIR__.'/views/header.php');
         <form name="postForm" onsubmit="return false;" class="col-lg-10 col-md-12">
           <fieldset class="form-group">
             <label for="formGroupTitle">Title</label>
-            <input type="text" class="form-control" id="formGroupTitle" placeholder="Your title" name="postTitle" required>
+            <input type="text" class="form-control" id="formGroupTitle" placeholder="The most descriptive title" name="postTitle" required>
           </fieldset>
           <fieldset class="form-group">
             <label for="formGroupLink">Link</label>
-            <input type="text" class="form-control" id="formGroupLink" placeholder="Your link" name="postLink">
+            <input type="text" class="form-control" id="formGroupLink" placeholder="Link to something awesome" name="postLink">
           </fieldset>
           <fieldset class="form-group">
             <label for="formGroupExampleInput2">Text</label>
-            <textarea rows="4" type="text" class="form-control" id="formGroupExampleInput2" placeholder="Your text" name="postText" required></textarea>
+            <textarea rows="4" type="text" class="form-control" id="formGroupExampleInput2" placeholder="An interesting story" name="postText" required></textarea>
           </fieldset>
           <input type="hidden" name="postAuthor" value="<?php echo $_SESSION['userid']; ?>">
           <input type="submit" name="postSubmit" value="Post" class="btn btn-primary">
