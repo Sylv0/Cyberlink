28f4bd099b954e6e496cc9f10dc6cd250daa1667
Johan Sylvan <gtc124@gmail.com>
2018-01-13 17:50:06 +0100

    Updated get functions to use post id's

--- app/posts/get.php
+++ app/posts/get.php
@@ -6,7 +6,7 @@
         die;
     }
     if(isset($_POST['getAll']) && $_POST['getAll']){
-        $getPosts = $pdo->prepare('SELECT p.title, p.postText, p.postTime, p.updateTime, p.image_url, u.nickname AS author FROM posts AS p, users AS u WHERE p.authorID=u.id');
+        $getPosts = $pdo->prepare('SELECT p.postID, p.title, p.postText, p.postTime, p.updateTime, p.image_url, u.nickname AS author FROM posts AS p, users AS u WHERE p.authorID=u.id');
         if(!$getPosts->execute()){
             echo json_encode(["error"=>true, "errorInfo"=>"Couldn't get posts."]);
             die;
@@ -14,7 +14,7 @@
         echo json_encode($getPosts->fetchAll(PDO::FETCH_ASSOC));
         die;
     }else if(isset($_POST['getLatest']) && $_POST['getLatest']){
-        $getPosts = $pdo->prepare('SELECT p.title, p.postText, p.postTime, p.updateTime, p.image_url, u.nickname AS author FROM posts AS p, users AS u WHERE p.authorID=u.id ORDER BY p.postID DESC LIMIT 1');
+        $getPosts = $pdo->prepare('SELECT p.postID, p.title, p.postText, p.postTime, p.updateTime, p.image_url, u.nickname AS author FROM posts AS p, users AS u WHERE p.authorID=u.id ORDER BY p.postID DESC LIMIT 1');
         if(!$getPosts->execute()){
             echo json_encode(["error"=>true, "errorInfo"=>"Couldn't get posts."]);
             die;
@@ -22,6 +22,8 @@
         echo json_encode($getPosts->fetch(PDO::FETCH_ASSOC));
         die;
     }else if(isset($_POST['getVotes']) && $_POST['getVotes']){
-        $getVotes = $pdo->prepare('SELECT * from postVotes');
+        $getVotes = $pdo->prepare('SELECT * FROM postVotes WHERE postID=:ID');
+        $getVotes->bindParam(':ID', $_POST['postID']);
+        $getVotes->execute();
         echo json_encode($getVotes->fetch(PDO::FETCH_ASSOC));
     }
