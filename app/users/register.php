cac72d3e1f998db88fda384b4384d5ed76c0998f
Johan Sylvan <gtc124@gmail.com>
2018-01-14 23:12:10 +0100

    Added registered date

--- app/users/register.php
+++ app/users/register.php
@@ -10,7 +10,7 @@ $user = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
 $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 $passw = password_hash(filter_var($_POST['passw'], FILTER_SANITIZE_STRING), PASSWORD_DEFAULT);
 
-$statement = $pdo->prepare("INSERT INTO users(nickname, email, passw) VALUES(:username, :email, :passw)");
+$statement = $pdo->prepare("INSERT INTO users(nickname, email, passw, regDate) VALUES(:username, :email, :passw, datetime())");
 $statement->bindParam(':username', $user, PDO::PARAM_STR);
 $statement->bindParam(':email', $email, PDO::PARAM_STR);
 $statement->bindParam(':passw', $passw, PDO::PARAM_STR);
