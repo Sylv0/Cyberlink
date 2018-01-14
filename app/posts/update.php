6e19f90ff65f2476f18271700cb54ac7d2e27591
Johan Sylvan <gtc124@gmail.com>
2018-01-14 23:16:20 +0100

    Moved user update to correct file

--- app/posts/update.php
+++ app/posts/update.php
@@ -3,22 +3,3 @@
 declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
-
-$getData = $pdo->prepare('SELECT * FROM users WHERE id=:id');
-$getData->bindParam(':id', $_SESSION['userid']);
-$getData->execute();
-$data = $getData->fetch(PDO::FETCH_ASSOC);
-
-$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-$bio = filter_var($_POST['bio'], FILTER_SANITIZE_STRING);
-$url = filter_var($_POST['avatar'], FILTER_SANITIZE_URL);
-$id = $_SESSION['userid'];
-
-$updateData = $pdo->prepare('UPDATE users SET email=:email, bio=:bio, avatar_url=:avatar WHERE id=:id');
-$updateData->bindParam(':email', $email);
-$updateData->bindParam(':bio', $bio);
-$updateData->bindParam(':avatar', $url);
-$updateData->bindParam(':id', $id);
-$updateData->execute();
-$pdo->errorInfo();
-redirect('../../profile.php');
--- app/users/update.php
+++ app/users/update.php
@@ -1,9 +1,24 @@
-<?php 
-    $getData = $pdo->prepare("SELECT * FROM users WHERE id=:id");
-    $getData->bindParam(':id', $_SESSION['userid']);
-    if(!$getData->execute()){
-        echo json_encode(['error'=>true, 'errorInfo'=>'An error occured while getting the user.']);
-    }
+<?php
 
-    $update = $pdo->prepare("UDATE users SET (email, bio, passw, avatar_url)");
-?>
+declare(strict_types=1);
+
+require __DIR__.'/../autoload.php';
+
+$getData = $pdo->prepare('SELECT * FROM users WHERE id=:id');
+$getData->bindParam(':id', $_SESSION['userid']);
+$getData->execute();
+$data = $getData->fetch(PDO::FETCH_ASSOC);
+
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$bio = filter_var($_POST['bio'], FILTER_SANITIZE_STRING);
+$url = filter_var($_POST['avatar'], FILTER_SANITIZE_URL);
+$id = $_SESSION['userid'];
+
+$updateData = $pdo->prepare('UPDATE users SET email=:email, bio=:bio, avatar_url=:avatar WHERE id=:id');
+$updateData->bindParam(':email', $email);
+$updateData->bindParam(':bio', $bio);
+$updateData->bindParam(':avatar', $url);
+$updateData->bindParam(':id', $id);
+$updateData->execute();
+$pdo->errorInfo();
+redirect('../../profile.php');
