d31b91221bf81b0d6da5d2a4cf096b9fde57b0e6
Johan Sylvan <gtc124@gmail.com>
2018-01-15 21:20:37 +0100

    Added checks for all updates and profile image file system

--- app/users/update.php
+++ app/users/update.php
@@ -4,21 +4,39 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
+$id = $_SESSION['userid'];
 $getData = $pdo->prepare('SELECT * FROM users WHERE id=:id');
-$getData->bindParam(':id', $_SESSION['userid']);
+$getData->bindParam(':id', $id);
 $getData->execute();
 $data = $getData->fetch(PDO::FETCH_ASSOC);
+if(strlen($_POST['email']) > 0 && $_POST['email'] != $data['email']){
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $updateEmail = $pdo->prepare('UPDATE users SET email=:email WHERE id=:id');
+    $updateEmail->bindParam(':email', $email);
+    $updateEmail->bindParam(':id', $id);
+    $updateEmail->execute();
+}
+if(strlen($_POST['bio']) > 0 && $_POST['bio'] != $data['bio']){
+    $bio = filter_var($_POST['bio'], FILTER_SANITIZE_STRING);
+    $updateBio = $pdo->prepare('UPDATE users SET bio=:bio WHERE id=:id');
+    $updateBio->bindParam(':bio', $bio);
+    $updateBio->bindParam(':id', $id);
+    $updateBio->execute();
+}
+if(isset($_FILES['avatar'])){
+    $target_dir = "users/".$data['nickname']."/";
+    $target_file = $target_dir . "profileImage.png";
 
-$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-$bio = filter_var($_POST['bio'], FILTER_SANITIZE_STRING);
-$url = filter_var($_POST['avatar'], FILTER_SANITIZE_URL);
-$id = $_SESSION['userid'];
+    if(!mkdir("../../".$target_dir, 0777, true)) echo "Folder already exists";
 
-$updateData = $pdo->prepare('UPDATE users SET email=:email, bio=:bio, avatar_url=:avatar WHERE id=:id');
-$updateData->bindParam(':email', $email);
-$updateData->bindParam(':bio', $bio);
-$updateData->bindParam(':avatar', $url);
-$updateData->bindParam(':id', $id);
-$updateData->execute();
-$pdo->errorInfo();
+    if (move_uploaded_file($_FILES["avatar"]["tmp_name"], "../../".$target_file)) {
+        $updateAvatar = $pdo->prepare('UPDATE users SET avatar_url=:avatar WHERE id=:id');
+        $updateAvatar->bindParam(':avatar', $target_file);
+        $updateAvatar->bindParam(':id', $id);
+        $updateAvatar->execute();
+        echo "The file ". basename( $_FILES["avatar"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
 redirect('../../profile.php');
