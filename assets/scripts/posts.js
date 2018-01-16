73e6f6f6fab85b23b5b93107d44c038a63c92158
Johan Sylvan <gtc124@gmail.com>
2018-01-16 11:50:59 +0100

    For an unknown goal

--- assets/scripts/posts.js
+++ assets/scripts/posts.js
@@ -54,9 +54,9 @@ const createPost = (post) => {
                 <footer class="blockquote-footer"><a href="profile.php?targetUser=${post['userID']}">${post['author']}</a> <cite title="Source Title">${post['postTime']}</cite> (${post['updateTime']})</footer>
             </blockquote>
             <blockquote class="col-2">
-                <a onclick="return false;" href="#" class="btn btn-sm btn-primary active vote" data-postid="${post['postID']}" data-vote="1">&uArr;</a>
+                <a href="#" class="btn btn-sm btn-primary active vote" data-postid="${post['postID']}" data-vote="1">&uArr;</a>
                 <span class="voteScore" data-postid="${post['postID']}">0</span>
-                <a onclick="return false;" href="#" class="btn btn-sm btn-primary active vote" data-postid="${post['postID']}" data-vote="-1">&dArr;</a>
+                <a href="#" class="btn btn-sm btn-primary active vote" data-postid="${post['postID']}" data-vote="-1">&dArr;</a>
             </blockquote>
         </div>
     </div>
@@ -67,6 +67,31 @@ const createPost = (post) => {
         div.querySelector('a.card-header').setAttribute('onclick', 'return false;');
         div.querySelector('a.card-header').innerHTML += "(no link)";
     }
+    div.querySelectorAll('a.vote').forEach(btn => {
+        btn.addEventListener('click', e => {
+            e.preventDefault();
+            let formData = new FormData();
+            formData.append('postID', btn.getAttribute('data-postid'));
+            formData.append('userID', document.querySelector('div[data-userid]').getAttribute("data-userid"));
+            formData.append('vote', btn.getAttribute('data-vote'));
+            var request = new Request('app/posts/vote.php', {
+                method: 'POST',
+                body: formData
+            })
+
+            fetch(request)
+            .then(function (data) {
+                return data.json();
+            })
+            .then(data => {
+                console.log(data);
+                if(data['voteSaved']){
+                    getVotes();
+                }
+            })
+            .catch(res => console.log(res));
+        });
+    });
     return div;
 }
 
@@ -130,12 +155,16 @@ const getVotes = (postID) => {
         return data.json();
     })
     .then(data => {
-        console.log(data);
+        document.querySelectorAll(`span.voteScore[data-postid]`).forEach(ele => {
+            ele.innerHTML = 0;
+        });
         data.forEach(vote => {
             let scoretarget = document.querySelector(`span.voteScore[data-postid="${vote['postID']}"]`);
             scoretarget.innerHTML = parseInt(scoretarget.innerHTML, 10)+parseInt(vote['vote'], 10);
-            let votebuttontarget = document.querySelector(`a.btn.vote[data-postid="${vote['postID']}"][data-vote="${vote['vote']}"]`);
-            votebuttontarget.classList.add('disabled');
+            if(vote['userID'] == document.querySelector('div[data-userid]').getAttribute('data-userid')){
+                let votebuttontarget = document.querySelector(`a.btn.vote[data-postid="${vote['postID']}"][data-vote="${vote['vote']}"]`);
+                votebuttontarget.classList.add('disabled');
+            }
         });
     })
     .catch(res => console.log(res));
