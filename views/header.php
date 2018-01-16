b9bc2ea190136a542b97f2aa617adac50ef56f0c
Johan Sylvan <gtc124@gmail.com>
2018-01-16 11:48:59 +0100

    What even is "working"?

--- views/header.php
+++ views/header.php
@@ -13,6 +13,10 @@ require(__DIR__.'/../app/autoload.php');
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/styles/styles.css">
+    <link rel="shortcut icon" type="image/png" href="https://assets.ngin.com/site_files/5591/favicon.ico"/>
   </head>
   <body>
+    <?php if(isset($_SESSION['userid'])): ?>
+    <div data-userid="<?php echo $_SESSION['userid'] ?>"></div>
+    <?php endif; ?>
     <?php require(__DIR__.'/navbar.php'); ?>
