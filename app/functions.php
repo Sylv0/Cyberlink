6f3787c05d7ca44cf4fd7d670e593824b2cba92a
Johan Sylvan <gtc124@gmail.com>
2018-02-07 19:03:38 +0100

    Added function to send in and execute a command in database

--- app/functions.php
+++ app/functions.php
@@ -16,3 +16,21 @@ if (!function_exists('redirect')) {
         exit;
     }
 }
+
+/**
+ * Give an SQL-statement with paramaters and get the result
+ *
+ * @param string $sql
+ * @param array $params
+ * @param bool $fetchAll
+ * @return void
+ */
+function SelectFromBD($pdo, string $sql, array $params, $fetchAll) : array{
+    $statement = $pdo->prepare($sql);
+    $statement->execute($params);
+    if($fetchAll){
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }else{
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+}
